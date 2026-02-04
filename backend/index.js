6fbf831f712a8a8177d575c03ad1db42f92b8308
Roman Casilas <osopv18@gmail.com>
2026-02-03 17:31:18 -0800

    Intial fullstack setup

---
+++ backend/index.js
@@ -0,0 +1,9 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  res.end("Hello from backend");
+});
+
+server.listen(3000, () => {
+  console.log("Server running on http://localhost:3000");
+});
